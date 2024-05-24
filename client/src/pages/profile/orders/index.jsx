8f17211a9eae25b017a7e5e510fbd2b6ccca8a32
RonniePlay <ronnieplayyt@gmail.com>
2024-05-24 13:47:16 +0300

    Update profile pages

--- client/src/pages/profile/orders/index.jsx
+++ client/src/pages/profile/orders/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import arrowImg from "../../../assets/images/profile/arrow.svg";
+import emptyImg from "../../../assets/images/profile/empty.png";
 import { useSelector } from "react-redux";
 
 export default function OrdersP() {
@@ -10,53 +11,59 @@ export default function OrdersP() {
     <>
       <h1>Мої замовлення</h1>
       <div className="pf-b">
-        {state?.orders?.length > 0
-          ? state.orders.map((item, index) => (
+        {state?.orders?.length > 0 ? (
+          state.orders.map((item, index) => (
+            <div
+              className={`profile-block ${
+                show.includes(item._id) ? "active" : ""
+              }`}
+              key={index}
+            >
               <div
-                className={`profile-block ${
-                  show.includes(item._id) ? "active" : ""
-                }`}
-                key={index}
+                className="profile-block-btn"
+                onClick={() =>
+                  setShow((prev) => {
+                    if (prev.includes(item._id))
+                      return prev.filter((f) => f !== item._id);
+                    else return [...prev, item._id];
+                  })
+                }
               >
-                <div
-                  className="profile-block-btn"
-                  onClick={() =>
-                    setShow((prev) => {
-                      if (prev.includes(item._id))
-                        return prev.filter((f) => f !== item._id);
-                      else return [...prev, item._id];
-                    })
-                  }
-                >
-                  <div>
-                    <h2>Замовлення #{index + 1}</h2>
-                  </div>
-                  <img src={arrowImg} alt="arrow" width={17} />
-                </div>
                 <div>
-                  {item?.productIds.map((item2, index2) => (
-                    <div className="profile-show" key={index2}>
-                      <div>{index2 + 1}</div>
-                      <div>
-                        <div className="profile-info">
-                          <div>Назва товару</div>
-                          <div>{item2.name}</div>
-                        </div>
-                        <div className="profile-info">
-                          <div>Кількість</div>
-                          <div>{item2.quantity}</div>
-                        </div>
-                        <div className="profile-info">
-                          <div>Сумма</div>
-                          <div>{item2.price}$</div>
-                        </div>
+                  <h2>Замовлення #{index + 1}</h2>
+                </div>
+                <img src={arrowImg} alt="arrow" width={17} />
+              </div>
+              <div>
+                {item?.productIds.map((item2, index2) => (
+                  <div className="profile-show" key={index2}>
+                    <div>{index2 + 1}</div>
+                    <div>
+                      <div className="profile-info">
+                        <div>Назва товару</div>
+                        <div>{item2.name}</div>
+                      </div>
+                      <div className="profile-info">
+                        <div>Кількість</div>
+                        <div>{item2.quantity}</div>
+                      </div>
+                      <div className="profile-info">
+                        <div>Сумма</div>
+                        <div>{item2.price}$</div>
                       </div>
                     </div>
-                  ))}
-                </div>
+                  </div>
+                ))}
               </div>
-            ))
-          : "Пусто"}
+            </div>
+          ))
+        ) : (
+          <div className="profile_empty">
+            <img src={emptyImg} alt="empty" />
+            <div>Список замовлень пустий</div>
+            <div>Ви ще нічого не замовляли</div>
+          </div>
+        )}
       </div>
     </>
   );
--- client/src/pages/profile/recall/index.jsx
+++ client/src/pages/profile/recall/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import arrowImg from "../../../assets/images/profile/arrow.svg";
+import emptyImg from "../../../assets/images/profile/empty.png";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
@@ -27,68 +28,72 @@ export default function RecallP() {
     <>
       <h1>Мої замовлення</h1>
       <div className="pf-b">
-        {data.length > 0
-          ? data.map((item) => (
+        {data.length > 0 ? (
+          data.map((item) => (
+            <div
+              className={`profile-block ${
+                show.includes(item._id) ? "active" : ""
+              }`}
+            >
               <div
-                className={`profile-block ${
-                  show.includes(item._id) ? "active" : ""
-                }`}
+                className="profile-block-btn"
+                onClick={() =>
+                  setShow((prev) => {
+                    if (prev.includes(item._id))
+                      return prev.filter((f) => f !== item._id);
+                    else return [...prev, item._id];
+                  })
+                }
               >
-                <div
-                  className="profile-block-btn"
-                  onClick={() =>
-                    setShow((prev) => {
-                      if (prev.includes(item._id))
-                        return prev.filter((f) => f !== item._id);
-                      else return [...prev, item._id];
-                    })
-                  }
-                >
-                  <div>
-                    <h2>{item.productName}</h2>
-                  </div>
-                  <img src={arrowImg} alt="arrow" width={17} />
-                </div>
                 <div>
-                  <div className="profile-show2">
-                    <div>
-                      <div className="profile-info">
-                        <div>Відгук на товар</div>
-                        <div>{item.message}</div>
+                  <h2>{item.productName}</h2>
+                </div>
+                <img src={arrowImg} alt="arrow" width={17} />
+              </div>
+              <div>
+                <div className="profile-show2">
+                  <div>
+                    <div className="profile-info">
+                      <div>Відгук на товар</div>
+                      <div>{item.message}</div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="profile-info">
+                      <div>Рейтинг</div>
+                      <div>
+                        {item.star}{" "}
+                        {item.star === 5
+                          ? "(Дуже добре)"
+                          : item.star === 4
+                          ? "(Добре)"
+                          : item.star === 3
+                          ? "(Нормально)"
+                          : item.star === 2
+                          ? "(Погано)"
+                          : item.star === 1
+                          ? "(Дуже погано)"
+                          : "(Ніякий)"}
                       </div>
                     </div>
-                    <div>
-                      <div className="profile-info">
-                        <div>Рейтинг</div>
-                        <div>
-                          {item.star}{" "}
-                          {item.star === 5
-                            ? "(Дуже добре)"
-                            : item.star === 4
-                            ? "(Добре)"
-                            : item.star === 3
-                            ? "(Нормально)"
-                            : item.star === 2
-                            ? "(Погано)"
-                            : item.star === 1
-                            ? "(Дуже погано)"
-                            : "(Ніякий)"}
-                        </div>
-                      </div>
-                      <div className="profile-info">
-                        <div>Дата залишення</div>
-                        <div>
-                          {new Date(item.dataCreate).toLocaleDateString(
-                            "ru-RU"
-                          )}
-                        </div>
+                    <div className="profile-info">
+                      <div>Дата залишення</div>
+                      <div>
+                        {new Date(item.dataCreate).toLocaleDateString("ru-RU")}
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            ))
-          : "Пусто"}
+            </div>
+          ))
+        ) : (
+          <div className="profile_empty">
+            <img src={emptyImg} alt="empty" />
+            <div>Відгуків ще немає</div>
+            <div>Ви ще не залишали відгуки про товари</div>
+          </div>
+        )}
       </div>
     </>
   );
